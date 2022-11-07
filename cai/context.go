8da261e4175285a52fb65f511e61da18bb898341
caijianhaoa <1784581164@qq.com>
2022-11-07 16:34:14 +0800

    实现前缀树路由

--- cai/context.go
+++ cai/context.go
@@ -13,6 +13,7 @@ type Context struct {
 	Req        *http.Request
 	Path       string
 	Method     string
+	Params     map[string]string
 	StatusCode int
 }
 
@@ -66,4 +67,9 @@ func (c *Context) HTML(code int, html string) {
 	c.SetHeader("Content-Type", "text/html")
 	c.Status(code)
 	c.Writer.Write([]byte(html))
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
 }
--- main.go
+++ main.go
@@ -10,16 +10,19 @@ func main() {
 	r.GET("/", func(c *cai.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello cai</h1>")
 	})
+
 	r.GET("/hello", func(c *cai.Context) {
 		// expect /hello?name=caiktutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.POST("/login", func(c *cai.Context) {
-		c.JSON(http.StatusOK, cai.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
-		})
+	r.GET("/hello/:name", func(c *cai.Context) {
+		// expect /hello/caijianhao
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *cai.Context) {
+		c.JSON(http.StatusOK, cai.H{"filepath": c.Param("filepath")})
 	})
 
 	r.Run(":9999")
