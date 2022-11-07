dc100019d05d0b9f4ed6caf920e5559c46ca471b
caijianhaoa <1784581164@qq.com>
2022-11-07 17:07:53 +0800

    分组控制

--- main.go
+++ main.go
@@ -7,23 +7,34 @@ import (
 
 func main() {
 	r := cai.New()
-	r.GET("/", func(c *cai.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello cai</h1>")
+	r.GET("/index", func(c *cai.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *cai.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello cai</h1>")
+		})
 
-	r.GET("/hello", func(c *cai.Context) {
-		// expect /hello?name=caiktutu
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *cai.Context) {
+			// expect /hello?name=caiktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *cai.Context) {
+			// expect /hello/caiktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *cai.Context) {
+			c.JSON(http.StatusOK, cai.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *cai.Context) {
-		// expect /hello/caijianhao
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *cai.Context) {
-		c.JSON(http.StatusOK, cai.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
