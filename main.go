a8baf2e627814321d05c92a54e4759cb3edde40c
caijianhaoa <1784581164@qq.com>
2022-11-07 18:50:52 +0800

    添加错误恢复功能

--- main.go
+++ main.go
@@ -2,48 +2,18 @@ package main
 
 import (
 	"cai"
-	"fmt"
-	"html/template"
 	"net/http"
-	"time"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := cai.New()
-	r.Use(cai.Logger())
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
-	stu1 := &student{Name: "caiktutu", Age: 20}
-	stu2 := &student{Name: "Jack", Age: 22}
+	r := cai.Default()
 	r.GET("/", func(c *cai.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "Hello caiktutu\n")
 	})
-	r.GET("/students", func(c *cai.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", cai.H{
-			"title":  "cai",
-			"stuArr": [2]*student{stu1, stu2},
-		})
-	})
-
-	r.GET("/date", func(c *cai.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", cai.H{
-			"title": "cai",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *cai.Context) {
+		names := []string{"caiktutu"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	r.Run(":9999")
